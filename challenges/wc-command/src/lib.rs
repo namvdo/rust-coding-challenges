7ecf351d1ce9d461c24683c728b5d68da91acf37
Nam Van Do <nam.dv@coccoc.com>
2024-10-07 07:39:57 +0700

    WIP wc command

--- challenges/wc-command/src/lib.rs
+++ challenges/wc-command/src/lib.rs
@@ -1,21 +1,49 @@
-use std::time::Instant;
-use std::io::{BufRead, BufReader};
-use std::fs::*;
+use core::num;
+use std::sync::atomic::AtomicUsize;
 use rand::Rng;
-use rayon::prelude::*;
+use rayon::{prelude::*, vec};
+use rust_coding_challenges::utils::read_text_file_from_args;
+use std::cmp::min;
+use std::{fs::*, os};
+use std::io::{BufRead, BufReader, Read};
+use std::sync::{mpsc, Arc};
+use std::thread;
+use std::time::Instant;
+use std::sync::atomic::Ordering;
 
-pub fn solve(file: File) -> usize {
-    let reader = BufReader::new(file);
-    let str = reader
-        .lines()
-        .filter_map(Result::ok)
-        .collect::<Vec<String>>()
-        .join("\n");
-    count_words(&str)
+pub fn solve(text: String) -> usize {
+    let num_threads = std::thread::available_parallelism().unwrap().get();
+    count_words_parallel(text, num_threads)
 }
 
-fn count_words(text: &str) -> usize {
-    text.lines()
-    .flat_map(|f| f.split_whitespace())
-    .count()
+fn count_words_parallel(text: String, num_threads: usize) -> usize {
+    let lines: Vec<String> = text.lines().map(|line| line.to_string()).collect();
+    let lines_per_thread = (lines.len() + num_threads - 1) / num_threads;
+    let total_word_count = Arc::new(AtomicUsize::new(0));
+    let mut handles = Vec::new();
+
+    for i in 0..num_threads {
+        let start = lines_per_thread * i;
+        let end = min(start + lines_per_thread, lines.len());
+
+        let total_word_count_clone = Arc::clone(&total_word_count);
+        let chunk = lines[start..end].to_vec();
+
+        let handle = thread::spawn(move || {
+            let count = count_words_in_chunk(&chunk);
+            total_word_count_clone.fetch_add(count, Ordering::Relaxed)
+        });
+        handles.push(handle);
+    }
+    for handle in handles {
+        handle.join().unwrap();
+    }
+    total_word_count.load(Ordering::Relaxed)
+}
+
+fn count_words_in_chunk(chunk: &[String]) -> usize {
+    chunk
+        .iter()
+        .flat_map(|line| line.split_whitespace())
+        .count()
 }
--- challenges/wc-command/src/main.rs
+++ challenges/wc-command/src/main.rs
@@ -1,13 +1,32 @@
 
 use rust_coding_challenges::utils::read_text_file_from_args;
 use wc_command::solve;
-use std::{fs::File, time::Instant};
+use std::{fs::File, io::{self, BufRead, BufReader, Read}, time::Instant};
 fn main() -> std::io::Result<()> {
+    // let start = Instant::now();
+    let file_path = "/Users/namdo/Desktop/learntocodetogether/random_1m_lines.txt";
+    let text = read_file_to_string(file_path)?;
     let start = Instant::now();
-    let file = File::open("/Users/namdo/Desktop/learntocodetogether/rust-coding-challenges/challenges/wc-command/tests/inputs/random_1m_lines.txt").expect("OK");
-    let lines = solve(file);
-    println!("total words: {:?}", lines);
+    let wc = solve(text);
+    println!("total words: {:?}", wc);
     println!("total time: {:?}", start.elapsed());
-    // Print results or perform other actions as needed
+    println!("OK: {}", 8503930 == wc);
     Ok(())
+}
+
+
+fn million_lines(n: usize, file_path: &str, new_file_path: &str) {
+
+}
+
+fn read_file_to_string(file_path: &str) -> io::Result<String>{
+    let file = File::open(file_path)?;
+    let mut reader = BufReader::new(file);
+    let mut buffer = String::new();
+    let mut content = String::new();
+    while reader.read_line(&mut buffer)? > 0 {
+        content.push_str(&buffer);
+        buffer.clear();
+    }
+    Ok(content)
 }
